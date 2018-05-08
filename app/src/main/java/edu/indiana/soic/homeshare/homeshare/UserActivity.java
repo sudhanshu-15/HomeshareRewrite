e937109244e614038b3c3809ff1988dfde0c29cb
Sudhanshu Siddh <ssiddh@iu.edu>
2018-05-08 15:20:03 -0400

    Logic for getting firebase token and sending it to homeshare server

--- app/src/main/java/edu/indiana/soic/homeshare/homeshare/UserActivity.java
+++ app/src/main/java/edu/indiana/soic/homeshare/homeshare/UserActivity.java
@@ -3,11 +3,14 @@ package edu.indiana.soic.homeshare.homeshare;
 import android.arch.lifecycle.ViewModelProvider;
 import android.arch.lifecycle.ViewModelProviders;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.databinding.DataBindingUtil;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 
+import com.google.firebase.iid.FirebaseInstanceId;
 import com.google.gson.Gson;
 import com.google.zxing.integration.android.IntentIntegrator;
 import com.google.zxing.integration.android.IntentResult;
@@ -15,6 +18,7 @@ import com.google.zxing.integration.android.IntentResult;
 import javax.inject.Inject;
 
 import dagger.android.AndroidInjection;
+import edu.indiana.soic.homeshare.homeshare.api.ParticipantToken;
 import edu.indiana.soic.homeshare.homeshare.data.model.Participant;
 import edu.indiana.soic.homeshare.homeshare.databinding.ActivityUserBinding;
 import edu.indiana.soic.homeshare.homeshare.viewmodel.UserActivityViewModel;
@@ -28,11 +32,14 @@ public class UserActivity extends AppCompatActivity {
     @Inject
     ViewModelProvider.Factory viewModelFactory;
 
+    @Inject
+    SharedPreferences sharedPreferences;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         AndroidInjection.inject(this);
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_user);
+//        setContentView(R.layout.activity_user);
         binding = DataBindingUtil.setContentView(this, R.layout.activity_user);
         userActivityViewModel = ViewModelProviders.of(this, viewModelFactory).get(UserActivityViewModel.class);
     }
@@ -51,6 +58,22 @@ public class UserActivity extends AppCompatActivity {
         }
     }
 
+    public void generateToken(View view) {
+        String token = FirebaseInstanceId.getInstance().getToken();
+        String pId = sharedPreferences.getString("pId", "");
+        if (token.length() != 0){
+            binding.firebaseToken.setText(token);
+            sharedPreferences.edit().putString("firebaseToken", token).apply();
+        }
+        Log.d("UserActivity", "generateToken: " + pId);
+        Log.d("UserActivity", "generateToken: " + token);
+        if (token.length() != 0 && pId.length() != 0){
+            Log.d("SendToken", "generateToken: ");
+            ParticipantToken participantToken = new ParticipantToken(pId, token);
+            userActivityViewModel.sendTokenToServer(participantToken);
+        }
+    }
+
     @Override
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
         IntentResult result = IntentIntegrator.parseActivityResult(requestCode, resultCode, data);
@@ -58,6 +81,7 @@ public class UserActivity extends AppCompatActivity {
             Gson gson = new Gson();
             participant = gson.fromJson(result.getContents(), Participant.class);
             binding.setParticipant(participant);
+            sharedPreferences.edit().putString("pId", participant.getParticipantId()).apply();
         }else {
             super.onActivityResult(requestCode, resultCode, data);
         }
--- app/src/main/java/edu/indiana/soic/homeshare/homeshare/api/ParticipantToken.java
+++ app/src/main/java/edu/indiana/soic/homeshare/homeshare/api/ParticipantToken.java
@@ -4,4 +4,9 @@ public class ParticipantToken {
 
     private String participantId;
     private String fbasetoken;
+
+    public ParticipantToken(String participantId, String fbasetoken) {
+        this.participantId = participantId;
+        this.fbasetoken = fbasetoken;
+    }
 }
